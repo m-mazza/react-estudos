9e93202af134b6754b62009452d06924779f43e9
Marcus Mazza <m-mazza@live.com>
2024-03-27 22:40:20 -0300

    ajustes do HERO e criação do viewport do storybook

--- projeto-autoescola/.storybook/preview.js
+++ projeto-autoescola/.storybook/preview.js
@@ -1,4 +1,59 @@
-/** @type { import('@storybook/react').Preview } */
+import React from "react";
+
+import GlobalStyle from "../src/components/GlobalStyle";
+import { BreakpointSize } from "../src/components/Breakpoints";
+
+export const decorators = [
+  (StoryFn) => (
+    <>
+      <GlobalStyle />
+      <StoryFn />
+    </>
+  ),
+];
+
+const viewports = {
+  extraSmall: {
+    name: "Portrait phone (default)",
+    styles: {
+      width: "360px",
+      height: "640px",
+    },
+  },
+
+  small: {
+    name: "Landscape Phone (sm)",
+    styles: {
+      width: "640px",
+      height: "360px",
+    },
+  },
+
+  medium: {
+    name: "Tablet (md)",
+    styles: {
+      width: "768px",
+      height: "1024px",
+    },
+  },
+
+  large: {
+    name: "Desktop (lg)",
+    styles: {
+      width: "1024px",
+      height: "1366px",
+    },
+  },
+
+  extraLarge: {
+    name: "Large Desktop (xl)",
+    styles: {
+      width: "1280px",
+      height: "800px",
+    },
+  },
+};
+
 const preview = {
   parameters: {
     controls: {
@@ -6,6 +61,9 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    viewport: {
+      viewports,
     },
   },
 };
